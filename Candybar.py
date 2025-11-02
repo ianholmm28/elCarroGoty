28e9a5223e5ab5d4b8d5ecfeacaf17cc8b408486
Valentino Cinti <cinti.valentinoo@gmail.com>
2025-11-02 02:29:33 -0300

    ComprasCompletado

--- Candybar.py
+++ Candybar.py
@@ -1,27 +1,53 @@
-def compracandybar():
-    Productos=CargarCandy()
-    print("Candy Bar")
-    print("Opciones de productos:")
-    print(Productos.keys)
+from validacionDatos import estaEntre, validarString
 
+def actualizarCandybar(id,cantidad):
+    try:
+        with open("archivosDeTexto/CandybarProductosTemp.csv","w") as archTemporal:
+            with open("archivosDeTexto/CandybarProductos.csv","r") as archViejo:
+                for linea in archViejo:
+                    archTemporal.write(linea)
+        with open("archivosDeTexto/CandybarProductosTemp.csv","r") as archTemporal:
+            with open("archivosDeTexto/CandybarProductos.csv","w") as archActualizado:
+                for linea in archTemporal:
+                    if linea[0] == id:
+                        id,nombre,precio,stock = linea.split(";")
+                        stock = int(stock)
+                        archActualizado.write(f"{id};{nombre};{precio};{stock-cantidad}\n")
+                    else:
+                        archActualizado.write(linea)
+        with open("archivosDeTexto/CandybarProductosTemp.csv","w"):
+            pass
+        print("Actualizacion de stock exitosa.")
+    except (IOError, OSError):
+        print("Error al abrir el archivo.")
 
+def compraCandybar():
+    try:
+        with open ("archivosDeTexto/CandybarProductos.csv","r") as arch:
+            producto = estaEntre(1,5,"\nSeleccione el producto a comprar (1-5): ")
+            for linea in arch:
+                if int(linea[0]) == producto:
+                    id,nombre,precio,stock = linea.split(";")
+                    precio, stock = int(precio),int(stock)
+            cantidad = estaEntre(1,stock,f"Ingrese la cantidad de {nombre} a comprar (1-{stock}): ")
+            total = precio*cantidad
+            respuesta = validarString(("s","si","n","no"),f"El costo total es de ${total}. Desea realizar la compra?: ")
+            if respuesta == "s" or respuesta == "si":
+                actualizarCandybar(id,cantidad)
+            compraMas = validarString(("s","si","n","no"),f"Desea realizar otra compra?: ")
+            if compraMas == "s" or compraMas == "si":
+                mostrarProductos()
+    except (IOError, OSError):
+        print("Error al abrir el archivo.")
 
+def mostrarProductos():
+    try:
+        with open ("archivosDeTexto/CandybarProductos.csv","r") as arch:
+            for linea in arch:
+                id,nombre,precio,stock = linea.split(";")
+                print(f"{id}. {nombre} - ${precio}")
+        compraCandybar()
+    except (IOError, OSError):
+        print("Error al abrir el archivo.")
 
-
-def CargarCandy():
-    Arch=open("CandybarProdutos.cvs", mode="wt", encoding="utf-8")
-    productosCandy={}
-    for linea in Arch:
-        prod=linea.strip().split("/")
-        Specs={"precio":int(prod[1]),"stock":int(prod[2]),"id":prod[3]}
-        productosCandy[prod[0]]=Specs
-        
-    Arch.close()
-    return productosCandy
-
-def guardar_candybar_en_archivo(candybar):
-    with open("CandybarProdutos.cvs", mode="wt", encoding="utf-8") as arch:
-        for producto, datos in candybar.items():
-            linea = f"{producto}/{datos['Precio']}/{datos['Stock']}/{datos['ID']}\n"
-            arch.write(linea)
-    
+mostrarProductos()
